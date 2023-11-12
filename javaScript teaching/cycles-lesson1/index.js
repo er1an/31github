02b0606be61d737d6c59c9e2626b670ff05f436e
Erlan Abdullaev <abdullaev.erlan.kk@gmail.com>
2023-11-12 15:33:24 +0600

    add change

--- javaScript teaching/cycles-lesson1/index.js
+++ javaScript teaching/cycles-lesson1/index.js
@@ -1,5 +1,5 @@
 for (let x = 1; x <= 9; x++) {
   for (let y = 1; y <= 9; y++) {
-    document.write(x + "x" + y + "=" + x * y + "<br>");
+    document.write(x + "X" + y + "=" + x * y + "<br>");
   }
 }
