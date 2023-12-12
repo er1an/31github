d6b7971d0f2de725d2ce89b5edeb7a126cebf58f
Erlan Abdullaev <abdullaev.erlan.kk@gmail.com>
2023-12-12 16:02:39 +0600

    introduction to Jquery

---
+++ javaScript teaching/introduction to Jquery/jquery/js/in.js
@@ -0,0 +1,39 @@
+// const link = document.querySelector("a");
+
+// console.log(link);
+
+
+
+// const link = $("a");
+
+// console.log(link);
+
+
+
+
+
+// console.log($("a"));
+// console.log($(".btn"));
+// console.log($("#hello"));
+
+// console.log($(".nav .btn"));
+
+
+// console.log($(".section > .block"));
+// console.log($("input"));
+// console.log($("input[type=text]"));
+
+
+// console.log($("input:even"));   //выводить нечетные из длина ....
+//console.log($("input:odd"));   //выводить четные из длина ....
+
+
+
+// console.log($("img:not(.logo img)"));
+
+
+// console.log($("li:has(a)"));
+
+// console.log($("li:not(a)"));
+// console.log($("li:has()"));
+console.log($("li:last"));
